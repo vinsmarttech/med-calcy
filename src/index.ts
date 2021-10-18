fa9456c94082d42071baced447eaaf0f7a508076
vinsmartdev <92749732+vinsmarttech@users.noreply.github.com>
2021-10-19 00:41:42 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,52 @@
-export function WeightConverter(
+export enum WEIGHT_UNIT {
+  LB,
+  KG,
+  G,
+  OZ,
+  POUND,
+  TON,
+}
+export function weightUnitConverter(
   weight: any,
-  convertFrom: string,
-  convertTo: string
+  unitFrom: WEIGHT_UNIT,
+  unitTo: WEIGHT_UNIT
 ) {
-  if (convertFrom === "KG") {
-    if (convertTo === "LB") {
-      return weight * 10;
-    }
-  } else {
-    return weight;
+  switch (unitFrom) {
+    case WEIGHT_UNIT.KG:
+      if (unitTo === WEIGHT_UNIT.G) {
+        return weight / 1000;
+      }
+      if (unitTo === WEIGHT_UNIT.LB) {
+        return weight * 2.20462;
+      } else if (unitTo === WEIGHT_UNIT.OZ) {
+        return weight * 2.20462;
+      }
+      if (unitTo === WEIGHT_UNIT.POUND) {
+        return weight * 2.20462;
+      }
+      if (unitTo === WEIGHT_UNIT.TON) {
+        return weight * 1000;
+      }
+      break;
+    case WEIGHT_UNIT.G:
+      weight = weight * 1000;
+      if (unitTo === WEIGHT_UNIT.KG) {
+        return weight;
+      }
+      if (unitTo === WEIGHT_UNIT.LB) {
+        return weight * 2.20462;
+      } else if (unitTo === WEIGHT_UNIT.OZ) {
+        return weight * 2.20462;
+      }
+      if (unitTo === WEIGHT_UNIT.POUND) {
+        return weight * 2.20462;
+      }
+      if (unitTo === WEIGHT_UNIT.TON) {
+        return weight * 1000;
+      }
+      break;
+
+    default:
+      return weight;
   }
 }
