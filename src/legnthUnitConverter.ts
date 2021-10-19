ac2eec2b52f2baf1a4202b96c542b59392f5c4d3
panindra2017 <30747713+panindra2017@users.noreply.github.com>
2021-10-19 17:07:04 +0530

    Update legnthUnitConverter.ts

--- src/legnthUnitConverter.ts
+++ src/legnthUnitConverter.ts
@@ -1,102 +1,120 @@
-export enum WEIGHT_UNIT {
-  LB,
-  KG,
-  G,
-  OZ,
-  POUND,
-  TON,
+export enum LENGTH_UNIT {
+  MM,
+  CM,
+  M,
+  KM,
+  INCH,
+  FEET
+
 }
 export function legnthUnitConverter(
-  weight: any,
-  unitFrom: WEIGHT_UNIT,
-  unitTo: WEIGHT_UNIT
+  length: any,
+  unitFrom: LENGTH_UNIT,
+  unitTo: LENGTH_UNIT
 ) {
   switch (unitFrom) {
-    case WEIGHT_UNIT.KG:
-      if (unitTo === WEIGHT_UNIT.G) {
-        return weight / 1000;
+    case LENGTH_UNIT.MM:
+      if (unitTo === LENGTH_UNIT.CM) {
+        return length * 0.1;
       }
-      if (unitTo === WEIGHT_UNIT.LB) {
-        return weight * 2.20462;
-      } else if (unitTo === WEIGHT_UNIT.OZ) {
-        return weight * 2.20462;
+      if (unitTo === LENGTH_UNIT.M) {
+        return length * 0.001;
+      } else if (unitTo === LENGTH_UNIT.KM) {
+        return length * 0.000001;
       }
-      if (unitTo === WEIGHT_UNIT.POUND) {
-        return weight * 2.20462;
+      if (unitTo === LENGTH_UNIT.INCH) {
+        return length * 0.0393701;
       }
-      if (unitTo === WEIGHT_UNIT.TON) {
-        return weight * 1000;
+      if (unitTo === LENGTH_UNIT.FEET) {
+        return length * 0.00328084;
       }
       break;
-    case WEIGHT_UNIT.G:
-      weight = weight * 1000;
-      if (unitTo === WEIGHT_UNIT.KG) {
-        return weight;
+    case LENGTH_UNIT.CM:
+      //length = length * 1000;
+      if (unitTo === LENGTH_UNIT.MM) {
+        return length * 10;
       }
-      if (unitTo === WEIGHT_UNIT.LB) {
-        return weight * 2.20462;
-      } else if (unitTo === WEIGHT_UNIT.OZ) {
-        return weight * 2.20462;
+      if (unitTo === LENGTH_UNIT.M) {
+        return length * 0.01;
+      } else if (unitTo === LENGTH_UNIT.KM) {
+        return length * 0.00001;
       }
-      if (unitTo === WEIGHT_UNIT.POUND) {
-        return weight * 2.20462;
+      if (unitTo === LENGTH_UNIT.INCH) {
+        return length * 0.393701;
       }
-      if (unitTo === WEIGHT_UNIT.TON) {
-        return weight * 1000;
+      if (unitTo === LENGTH_UNIT.FEET) {
+        return length * 0.0328084;
       }
       break;
  
-    case WEIGHT_UNIT.LB:
-      if (unitTo === WEIGHT_UNIT.KG) {
-        return weight / 2.20462;
+    case LENGTH_UNIT.M:
+      if (unitTo === LENGTH_UNIT.MM) {
+        return length * 1000;
       }
-      if (unitTo === WEIGHT_UNIT.G) {
-        return (weight / 2.20462) * 0.001;
-      } else if (unitTo === WEIGHT_UNIT.OZ) {
-        return weight * 16;
+      if (unitTo === LENGTH_UNIT.CM) {
+        return (length * 100);
+      } else if (unitTo === LENGTH_UNIT.KM) {
+        return length * 0.001;
       }
-      if (unitTo === WEIGHT_UNIT.POUND) {
-        return weight;
+      if (unitTo === LENGTH_UNIT.INCH) {
+        return length * 39.3701;
       }
-      if (unitTo === WEIGHT_UNIT.TON) {
-        return weight * 0.000453592;
-      }
-      break;
-    case WEIGHT_UNIT.OZ:
-      if (unitTo === WEIGHT_UNIT.KG) {
-        return weight * 0.0283495;
-      }
-      if (unitTo === WEIGHT_UNIT.G) {
-        return weight * 28.3495;
-      } else if (unitTo === WEIGHT_UNIT.LB) {
-        return weight * 0.0625;
-      }
-      if (unitTo === WEIGHT_UNIT.POUND) {
-        return weight * 0.0625;
-      }
-      if (unitTo === WEIGHT_UNIT.TON) {
-        return weight * 0.00002835;
+      if (unitTo === LENGTH_UNIT.FEET) {
+        return length * 3.28084;
       }
       break;
-    case WEIGHT_UNIT.TON:
-      if (unitTo === WEIGHT_UNIT.KG) {
-        return weight * 1000;
+    case LENGTH_UNIT.KM:
+      if (unitTo === LENGTH_UNIT.MM) {
+        return length * 1000000;
       }
-      if (unitTo === WEIGHT_UNIT.G) {
-        return weight * 1000 * 0.001;
-      } else if (unitTo === WEIGHT_UNIT.LB) {
-        return weight * 2204.62;
+      if (unitTo === LENGTH_UNIT.CM) {
+        return length * 100000;
+      } else if (unitTo === LENGTH_UNIT.M) {
+        return length * 1000;
       }
-      if (unitTo === WEIGHT_UNIT.POUND) {
-        return weight * 2204.62;
+      if (unitTo === LENGTH_UNIT.INCH) {
+        return length * 39370.1;
       }
-      if (unitTo === WEIGHT_UNIT.OZ) {
-        return weight * 35274;
+      if (unitTo === LENGTH_UNIT.FEET) {
+        return length * 3280.84;
+      }
+      break;
+    case LENGTH_UNIT.INCH:
+      if (unitTo === LENGTH_UNIT.MM) {
+        return length * 25.4;
+      }
+      if (unitTo === LENGTH_UNIT.CM) {
+        return length * 2.54;
+      } else if (unitTo === LENGTH_UNIT.M) {
+        return length * 0.0254;
+      }
+      if (unitTo === LENGTH_UNIT.KM) {
+        return length * 0.0000254;
+      }
+      if (unitTo === LENGTH_UNIT.FEET) {
+        return length * 0.0833333;
+      }
+      break;
+
+    case LENGTH_UNIT.FEET:
+      if (unitTo === LENGTH_UNIT.MM) {
+        return length * 304.8;
+      }
+      if (unitTo === LENGTH_UNIT.CM) {
+        return length * 30.48;
+      } else if (unitTo === LENGTH_UNIT.M) {
+        return length * 0.3048;
+      }
+      if (unitTo === LENGTH_UNIT.KM) {
+        return length * 0.0003048;
+      }
+      if (unitTo === LENGTH_UNIT.INCH) {
+        return length * 12;
       }
       break;
  
 
     default:
-      return weight;
+      return length;
   }
 }
