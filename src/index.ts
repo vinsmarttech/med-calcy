45f47239b01433278220676a083080bd6dac7457
panindra2017 <30747713+panindra2017@users.noreply.github.com>
2021-10-28 11:16:19 +0530

    update

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-import { lengthUnitConverter,LENGTH_UNIT } from "./lengthUnitConverter";
-import { Resus } from "./resus";
-import { weightUnitConverter,WEIGHT_UNIT } from "./weightUnitConverter";
+import { lengthUnitConverter, LENGTH_UNIT } from "./lengthUnitConverter";
+import * as Resus from "./resus";
+import { weightUnitConverter, WEIGHT_UNIT } from "./weightUnitConverter";
 
- export ={
-  weightUnitConverter,WEIGHT_UNIT,
-  lengthUnitConverter,LENGTH_UNIT,
-  Resus
-}
+export = {
+  weightUnitConverter,
+  WEIGHT_UNIT,
+  lengthUnitConverter,
+  LENGTH_UNIT,
+  Resus,
+};
